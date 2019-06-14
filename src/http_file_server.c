19c48d5d6eb60b77dff133e779f1f46c10a14168
cybaj <cybaj@naver.com>
2019-06-14 22:53:50 +0900

    first commit

---
+++ src/http_file_server.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <sys/socket.h> // socket, bind, listen, accept call.
+#include <unistd.h>
+#include <stdlib.h> // 프로세스 제어 exit 함수 
+#include <netinet/in.h> // sockaddr 구조체. 정확히는 sockaddr_in
+#include <string.h> // memset, strlen 함수
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    int server_fd, new_socket; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    long fsize;
+    FILE *fp = fopen("index.html", "r");
+    if (!fp)
+    {
+        perror("The file was not opened.");
+        exit(1);
+    }
+    printf("The file was opend.");
+
+    if( fseek(fp, 0, SEEK_END) == -1)
+    {
+        perror("The file was not seeked.");
+        exit(1);
+    }
+
+    fsize = ftell(fp);
+    if (fsize == -1)
+    {
+        perror("The file buffer was not allocated.");
+        exit(1);
+    }
+    rewind(fp);
+    char *send_buffer = (char *)malloc(fsize);
+
+    if (fread(send_buffer, fsize, 1, fp) != 1)
+    {
+        perror("The file was not read.");
+        exit(1);
+    }
+    fclose(fp);
+
+    printf("The file size is %ld\n", fsize);
+    
+    char hello[3000] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n";
+    strcat(hello, send_buffer);
+    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+    
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , hello , strlen(hello));
+        printf("------------------Hello message sent-------------------\n");
+        close(new_socket);
+    }
+    return 0;
+}
+
